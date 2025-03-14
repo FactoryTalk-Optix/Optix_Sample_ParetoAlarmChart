8fabdac16fc85b5226ef7f4a4d951d2ca019c5c5
asemlucben <>
2025-03-14 01:28:42 +0000

    Support to show echart in WPE

--- ProjectFiles/NetSolution/ParetoChartLogic.cs
+++ ProjectFiles/NetSolution/ParetoChartLogic.cs
@@ -46,103 +46,120 @@ public class ParetoChartLogic : BaseNetLogic
     [ExportMethod]
     public void RefreshGraph()
     {
-        ResourceUri pathHtml = ResourceUri.FromProjectRelativePath(@"eCharts\pareto.html");
-        GenerateHtmlFile(pathHtml.Uri, Owner.GetVariable("DarkMode").Value);
-        WebBrowser mainWebBrowser = (WebBrowser) Owner;
+        var pathHtml = ResourceUri.FromProjectRelativePath("eCharts/pareto.html");
+        var pathJs = ResourceUri.FromProjectRelativePath("eCharts/paretoData.js");
+        // Need to generate the JavaScript with Pareto chart script, for avoid issue on WebPresentationEngine
+        GenerateJavaScriptFile(pathJs.Uri, Owner.GetVariable("DarkMode").Value);
+        GenerateHtmlFile(pathHtml.Uri);
+        var mainWebBrowser = (WebBrowser) Owner;
         mainWebBrowser.URL = pathHtml;
         mainWebBrowser.Refresh();
     }
 
-    private void GenerateHtmlFile(string fullPath, bool darkMode)
+    private static void GenerateHtmlFile(string fullPath)
     {
-        StringBuilder newHtmlFile = new StringBuilder();
-        GenerateHtmlHead(ref newHtmlFile);
-        GenerateData(out string alarmNames, out string barValues, out string lineValues, out int maxCount);
-        string darkModeParameter = darkMode ? "'dark'" : "null";
+        var newHtmlFile = new StringBuilder();
+        _ = newHtmlFile.AppendLine("<!DOCTYPE html>");
+        _ = newHtmlFile.AppendLine("<html lang=\"en\" style=\"height: 100%\">");
+        _ = newHtmlFile.AppendLine("<head>");
+        _ = newHtmlFile.AppendLine("<meta charset=\"utf-8\">");
+        _ = newHtmlFile.AppendLine("</head>");
         _ = newHtmlFile.AppendLine("<body style=\"height: 100%; margin: 0\">");
-        _ = newHtmlFile.AppendLine("<div id=\"container\" style=\"height: 100%\"></div>");
-        _ = newHtmlFile.AppendLine("<script type=\"text/javascript\">");
-        // Javascript
-        _ = newHtmlFile.AppendLine("var dom = document.getElementById('container');");
-        _ = newHtmlFile.AppendLine($"var myChart = echarts.init(dom, {darkModeParameter}, {{ renderer: 'svg' }});");
-        _ = newHtmlFile.AppendLine("// Specify the configuration items and data for the chart");
-        _ = newHtmlFile.AppendLine("var option = {");
-        _ = newHtmlFile.AppendLine("tooltip: {");
-        _ = newHtmlFile.AppendLine("trigger: 'axis',");
-        _ = newHtmlFile.AppendLine("axisPointer: {");
-        _ = newHtmlFile.AppendLine("type: 'cross',");
-        _ = newHtmlFile.AppendLine("crossStyle: {");
-        _ = newHtmlFile.AppendLine("color: '#999'");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("},");
-        _ = newHtmlFile.AppendLine("legend: {");
-        _ = newHtmlFile.AppendLine("data: ['Count', 'Incidence']");
-        _ = newHtmlFile.AppendLine("},");
-        _ = newHtmlFile.AppendLine("xAxis: {");
-        _ = newHtmlFile.AppendLine("type: 'category',");
-        _ = newHtmlFile.AppendLine("data: " + alarmNames + ",");
-        _ = newHtmlFile.AppendLine("axisPointer: {");
-        _ = newHtmlFile.AppendLine("type: 'shadow'");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("},");
-        _ = newHtmlFile.AppendLine("yAxis: [");
-        _ = newHtmlFile.AppendLine("{");
-        _ = newHtmlFile.AppendLine("type: 'value',");
-        _ = newHtmlFile.AppendLine("name: 'Count',");
-        _ = newHtmlFile.AppendLine("min: 0,");
-        _ = newHtmlFile.AppendLine($"max: {maxCount + 10},");
-        _ = newHtmlFile.AppendLine("interval: 10,");
-        _ = newHtmlFile.AppendLine("axisLabel: {");
-        _ = newHtmlFile.AppendLine("formatter: '{value}'");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("},");
-        _ = newHtmlFile.AppendLine("{");
-        _ = newHtmlFile.AppendLine("type: 'value',");
-        _ = newHtmlFile.AppendLine("name: 'Incidence',");
-        _ = newHtmlFile.AppendLine("min: 0,");
-        _ = newHtmlFile.AppendLine("max: 100,");
-        _ = newHtmlFile.AppendLine("interval: 25,");
-        _ = newHtmlFile.AppendLine("axisLabel: {");
-        _ = newHtmlFile.AppendLine("formatter: '{value} %'");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("],");
-        _ = newHtmlFile.AppendLine("series: [");
-        _ = newHtmlFile.AppendLine("{");
-        _ = newHtmlFile.AppendLine("name: 'Count',");
-        _ = newHtmlFile.AppendLine("type: 'bar',");
-        _ = newHtmlFile.AppendLine("data: " + barValues);
-        _ = newHtmlFile.AppendLine("},");
-        _ = newHtmlFile.AppendLine("{");
-        _ = newHtmlFile.AppendLine("name: 'Incidence',");
-        _ = newHtmlFile.AppendLine("type: 'line',");
-        _ = newHtmlFile.AppendLine("yAxisIndex: 1,");
-        _ = newHtmlFile.AppendLine("tooltip: {");
-        _ = newHtmlFile.AppendLine("valueFormatter: function (value) {");
-        _ = newHtmlFile.AppendLine("return value + ' %';");
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("},");
-        _ = newHtmlFile.AppendLine("data: " + lineValues);
-        _ = newHtmlFile.AppendLine("}");
-        _ = newHtmlFile.AppendLine("]");
-        _ = newHtmlFile.AppendLine("};");
-        _ = newHtmlFile.AppendLine("myChart.setOption(option);");
-        _ = newHtmlFile.AppendLine("window.addEventListener('resize', myChart.resize);");
-        _ = newHtmlFile.AppendLine("</script>");
+        _ = newHtmlFile.AppendLine("<div id=\"container\" style=\"height: 100%; font: Share Tech Mono;\"></div>");
+        _ = newHtmlFile.AppendLine("<script type=\"text/javascript\" src=\"./echarts.js\"></script>");
+        _ = newHtmlFile.AppendLine("<script type=\"text/javascript\" src=\"./paretoData.js\"></script>");
         _ = newHtmlFile.AppendLine("</body>");
         _ = newHtmlFile.AppendLine("</html>");
         System.IO.File.WriteAllText(fullPath, newHtmlFile.ToString());
     }
 
-    private void GenerateHtmlHead(ref StringBuilder newHtmlFile)
+    private void GenerateJavaScriptFile(string filePath, bool darkMode)
     {
-        _ = newHtmlFile.AppendLine("<!DOCTYPE html>");
-        _ = newHtmlFile.AppendLine("<html lang=\"en\" style=\"height: 100%\">");
-        _ = newHtmlFile.AppendLine("<head>");
-        _ = newHtmlFile.AppendLine("<meta charset=\"utf-8\" />");
-        _ = newHtmlFile.AppendLine("<script src=\"echarts.js\"></script>");
-        _ = newHtmlFile.AppendLine("</head>");
+        StringBuilder jsFileContent = new();
+        GenerateData(out string alarmNames, out string barValues, out string lineValues, out int maxCount);
+        string darkModeParameter = darkMode ? "'dark'" : "null";
+
+        _ = jsFileContent.AppendLine("document.addEventListener('DOMContentLoaded', function() {");
+        _ = jsFileContent.AppendLine("    var dom = document.getElementById('container');");
+        _ = jsFileContent.AppendLine($"    var myChart = echarts.init(dom, {darkModeParameter}, {{ renderer: 'svg' }});");
+        _ = jsFileContent.AppendLine("    var option = {");
+        _ = jsFileContent.AppendLine("        tooltip: {");
+        _ = jsFileContent.AppendLine("            trigger: 'axis',");
+        _ = jsFileContent.AppendLine("            axisPointer: {");
+        _ = jsFileContent.AppendLine("                type: 'cross',");
+        _ = jsFileContent.AppendLine("                crossStyle: {");
+        _ = jsFileContent.AppendLine("                    color: '#999'");
+        _ = jsFileContent.AppendLine("                }");
+        _ = jsFileContent.AppendLine("            }");
+        _ = jsFileContent.AppendLine("        },");
+        GenerateJavaParetoData(ref jsFileContent, alarmNames, barValues, lineValues, maxCount);
+        _ = jsFileContent.AppendLine("    };");
+        _ = jsFileContent.AppendLine("    myChart.setOption(option);");
+        _ = jsFileContent.AppendLine("    window.addEventListener('resize', myChart.resize);");
+        _ = jsFileContent.AppendLine("});");
+
+        System.IO.File.WriteAllText(filePath, jsFileContent.ToString());
+    }
+
+    private static void GenerateJavaParetoData(ref StringBuilder jsFileContent, string alarmNames, string barValues, string lineValues, int maxCount)
+    {
+        _ = jsFileContent.AppendLine("        legend: {");
+        _ = jsFileContent.AppendLine("            data: ['Count', 'Incidence']");
+        _ = jsFileContent.AppendLine("        },");
+        _ = jsFileContent.AppendLine("        xAxis: {");
+        _ = jsFileContent.AppendLine("            type: 'category',");
+        _ = jsFileContent.AppendLine("            data: " + alarmNames + ",");
+        _ = jsFileContent.AppendLine("            axisPointer: {");
+        _ = jsFileContent.AppendLine("                type: 'shadow'");
+        _ = jsFileContent.AppendLine("            }");
+        _ = jsFileContent.AppendLine("        },");
+        _ = jsFileContent.AppendLine("        yAxis: [");
+        _ = jsFileContent.AppendLine("            {");
+        _ = jsFileContent.AppendLine("                type: 'value',");
+        _ = jsFileContent.AppendLine("                name: 'Count',");
+        _ = jsFileContent.AppendLine("                min: 0,");
+        _ = jsFileContent.AppendLine($"                max: {maxCount + 10},");
+        _ = jsFileContent.AppendLine("                interval: 10,");
+        _ = jsFileContent.AppendLine("                axisLabel: {");
+        _ = jsFileContent.AppendLine("                    formatter: '{value}'");
+        _ = jsFileContent.AppendLine("                }");
+        _ = jsFileContent.AppendLine("            },");
+        _ = jsFileContent.AppendLine("            {");
+        _ = jsFileContent.AppendLine("                type: 'value',");
+        _ = jsFileContent.AppendLine("                name: 'Incidence',");
+        _ = jsFileContent.AppendLine("                min: 0,");
+        _ = jsFileContent.AppendLine("                max: 100,");
+        _ = jsFileContent.AppendLine("                interval: 25,");
+        _ = jsFileContent.AppendLine("                axisLabel: {");
+        _ = jsFileContent.AppendLine("                    formatter: '{value} %'");
+        _ = jsFileContent.AppendLine("                }");
+        _ = jsFileContent.AppendLine("            }");
+        _ = jsFileContent.AppendLine("        ],");
+        _ = jsFileContent.AppendLine("        series: [");
+        _ = jsFileContent.AppendLine("            {");
+        _ = jsFileContent.AppendLine("                name: 'Count',");
+        _ = jsFileContent.AppendLine("                type: 'bar',");
+        _ = jsFileContent.AppendLine("                data: " + barValues);
+        _ = jsFileContent.AppendLine("            },");
+        _ = jsFileContent.AppendLine("            {");
+        _ = jsFileContent.AppendLine("                name: 'Incidence',");
+        _ = jsFileContent.AppendLine("                type: 'line',");
+        _ = jsFileContent.AppendLine("                yAxisIndex: 1,");
+        _ = jsFileContent.AppendLine("                tooltip: {");
+        _ = jsFileContent.AppendLine("                    valueFormatter: function (value) {");
+        _ = jsFileContent.AppendLine("                        return value + ' %';");
+        _ = jsFileContent.AppendLine("                    }");
+        _ = jsFileContent.AppendLine("                },");
+        _ = jsFileContent.AppendLine("                data: " + lineValues);
+        _ = jsFileContent.AppendLine("            }");
+        _ = jsFileContent.AppendLine("        ]");
+    }
+
+    private static string GenerateNonce()
+    {
+        byte[] nonceBytes = new byte[16];
+        System.Security.Cryptography.RandomNumberGenerator.Fill(nonceBytes);
+        return Convert.ToBase64String(nonceBytes);
     }
 
     public void GenerateData(out string alarmNames, out string barValues, out string lineValues, out int maxCount)
